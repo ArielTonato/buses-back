d7ebf7b5d963b497dd8e537eac228cefd63a7acd
Alexa <alexarieltonato@hotmail.com>
2024-11-19 18:40:02 -0500

    Se define la entidad create-user-dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,46 @@
-import { IsInt, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
-import { IsEcuadorianId } from "src/common/decorators/cedula.validator";
+import { 
+    IsEmail, 
+    IsOptional, 
+    IsPhoneNumber,  
+    IsString, 
+    MinLength 
+} from "class-validator";
+import { IsEcuadorianId } from "../../common/decorators/cedula.validator";
 
 export class CreateUserDto {
     @IsString()
-    @IsEcuadorianId({message:"La cedula ingresada no es valida"})
-    identification: string;
+    @IsEcuadorianId({ message: "La cédula ingresada no es válida" })
+    identificacion: string;
 
-    
+    @IsString()
+    @MinLength(3, { message: "El primer nombre debe tener al menos 3 caracteres" })
+    primer_nombre: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(3, { message: "El segundo nombre debe tener al menos 3 caracteres" })
+    segundo_nombre?: string;
+
+    @IsString()
+    @MinLength(3, { message: "El primer apellido debe tener al menos 3 caracteres" })
+    primer_apellido: string;
+
+    @IsOptional()
+    @IsString()
+    @MinLength(3, { message: "El segundo apellido debe tener al menos 3 caracteres" })
+    segundo_apellido?: string;
+
+    @IsEmail({}, { message: "El correo ingresado no es válido" })
+    correo: string;
+
+    @IsString()
+    @MinLength(6, { message: "La contraseña debe tener al menos 6 caracteres" })
+    password: string;
+
+    @IsPhoneNumber("EC", { message: "El número de teléfono debe ser válido para Ecuador" })
+    telefono: string;
+
+    @IsString()
+    @MinLength(5, { message: "La dirección debe tener al menos 5 caracteres" })
+    direccion: string;
 }
