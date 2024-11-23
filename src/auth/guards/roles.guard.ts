4f7be69a995bfd93680fdbd60aff6aa8c8726ef2
Alexa <alexarieltonato@hotmail.com>
2024-11-23 18:00:19 -0500

    Creacion del guard para autorizar los roles

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { ROLES_KEY } from "src/common/decorators/roles.decorator";
+import { Roles } from "src/common/enums/roles.enum";
+
+@Injectable()
+export class RolesGuard implements CanActivate{
+    constructor(
+        private readonly reflector: Reflector
+    ){}
+    canActivate(context: ExecutionContext): boolean  {
+        const rol = this.reflector.getAllAndOverride<Roles>(ROLES_KEY,[
+            context.getHandler(),
+            context.getClass()
+        ]);
+
+        console.log(rol);
+        if(!rol){
+            return true;
+        }
+        const user = context.switchToHttp().getRequest();
+        console.log(user);
+        if(user.rol === Roles.ADMINISTRADORES){
+            return true;
+        }
+
+        return rol === user.rol;
+    }
+}
