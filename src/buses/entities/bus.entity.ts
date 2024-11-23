525ec17a4002faa1ab8a6241a191099f67ae95e4
Alexa <alexarieltonato@hotmail.com>
2024-11-23 18:44:18 -0500

    Se crea la entidad buses

--- src/buses/entities/bus.entity.ts
+++ src/buses/entities/bus.entity.ts
@@ -1,1 +1,36 @@
-export class Bus {}
+import { Column, DeleteDateColumn, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Bus {
+    @PrimaryGeneratedColumn()
+    bus_id: number;
+
+    @Column()
+    numero_bus: number;
+
+    @Column()
+    placa: string;
+
+    @Column()
+    chassis: string;
+
+    @Column()
+    carroceria: string;
+
+
+    @Column()
+    total_asientos_normales: number;
+
+    @Column()
+    total_asientos_vip: number;
+
+    @Column()
+    activo: boolean;
+
+    @DeleteDateColumn()
+    fecha_eliminacion: Date;
+
+    //La fecha de creacion es un timestamp que se genera automaticamente
+    @Column({type:"timestamp", default: () => "CURRENT_TIMESTAMP"})
+    fecha_creacion: Date;
+}
