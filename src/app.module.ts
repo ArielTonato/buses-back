5f7974a7bb8c4172f8541c381a7fe1728e96960d
Alexa <alexarieltonato@hotmail.com>
2024-11-23 18:34:56 -0500

    Se crea el recurso buses

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { BusesModule } from './buses/buses.module';
 
 
 @Module({
@@ -17,7 +18,8 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
     UserModule,
-    AuthModule
+    AuthModule,
+    BusesModule
   ],
   controllers: [],
   providers: [],
