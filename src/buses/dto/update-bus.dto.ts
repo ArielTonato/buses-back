f62c6a0303d169a1620669547b395c16bc95782c
Alexa <alexarieltonato@hotmail.com>
2024-11-23 20:16:29 -0500

    Fix 1 buses

--- src/buses/dto/update-bus.dto.ts
+++ src/buses/dto/update-bus.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBusDto } from './create-bus.dto';
+import { IsBoolean } from 'class-validator';
 
-export class UpdateBusDto extends PartialType(CreateBusDto) {}
+export class UpdateBusDto extends PartialType(CreateBusDto) {
+    @IsBoolean()
+    activo: boolean
+}
