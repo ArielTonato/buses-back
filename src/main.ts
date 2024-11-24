926bb32cd26b7c787b9b1ace49bfe1f3fbc434e6
Alexa <alexarieltonato@hotmail.com>
2024-11-23 19:24:37 -0500

    Se crea la semmilla para cargar usuarios

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { runSeeds } from './seeds/seed';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,11 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   )
+
+  // Run database seeds
+  const dataSource = app.get(DataSource);
+  await runSeeds(dataSource);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
