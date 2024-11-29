3873be2a8fb9f71dae23a68cf8058477a46585e5
Alexa <alexarieltonato@hotmail.com>
2024-11-28 20:19:24 -0500

    Se crea el enum para los asientos y la entidad de asientos

--- src/asientos/entities/asiento.entity.ts
+++ src/asientos/entities/asiento.entity.ts
@@ -1,1 +1,24 @@
-export class Asiento {}
+import { Bus } from "src/buses/entities/bus.entity";
+import { Asientos } from "src/common/enums/asientos.enum";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Asiento {
+    @PrimaryGeneratedColumn()
+    asiento_id: number;
+
+    @Column({type:"enum", default:Asientos.NORMAL, enum:Asientos})
+    tipo_asiento: Asientos;
+
+    @Column()
+    numero_asiento: number;
+
+    @Column()
+    precio_base: number;
+
+    @Column({default: true})
+    activo: boolean;
+
+    @Column({type:"timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP"})
+    fecha_creacion: Date;
+}
