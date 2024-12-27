50e94e4273eeb9cf4a5ee06a3a859e0ef9dbec92
Alexa <alexarieltonato@hotmail.com>
2024-12-27 13:32:24 -0500

    Se agrega validacion para que no se envien solo espacios

--- src/comprobantes_pagos/dto/update-comprobantes_pago.dto.ts
+++ src/comprobantes_pagos/dto/update-comprobantes_pago.dto.ts
@@ -1,13 +1,17 @@
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
 import { EstadoComprobante } from '../../common/enums/comprobantes.enum';
+import { Transform } from 'class-transformer';
 
 export class UpdateComprobantesPagoDto {
+
     @IsEnum(EstadoComprobante)
     @IsOptional()
     estado?: EstadoComprobante;
 
     @IsString()
     @IsOptional()
+    @Transform(({ value }) => value.trim())
+    @MinLength(5,{message: 'El comentario debe tener al menos 5 caracteres'})
     comentarios?: string;
 
     @IsString()
