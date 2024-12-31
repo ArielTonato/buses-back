816eeb5b2c94fb3c75238bc1fe3f6d5c2a97cc2b
Alexa <alexarieltonato@hotmail.com>
2024-12-31 13:58:59 -0500

    Se crea la entidad y el DTO

--- src/cooperativa/dto/create-cooperativa.dto.ts
+++ src/cooperativa/dto/create-cooperativa.dto.ts
@@ -1,1 +1,27 @@
-export class CreateCooperativaDto {}
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreateCooperativaDto {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string;
+
+    @IsString()
+    @IsNotEmpty()
+    telefono: string;
+
+    @IsString()
+    @IsNotEmpty()
+    correo: string;
+
+    @IsString()
+    @IsNotEmpty()
+    logo: string;
+
+    @IsString()
+    @IsNotEmpty()
+    ruc: string;
+
+    @IsString()
+    @IsNotEmpty()
+    direccion: string;
+}
