d5d0bd4794a78be7541a67eb244d18b169b00307
Alexa <alexarieltonato@hotmail.com>
2025-01-01 15:18:44 -0500

    Se elimina codigo que no se necesita

--- src/buses-fotos/buses-fotos.service.ts
+++ src/buses-fotos/buses-fotos.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBusesFotoDto } from './dto/create-buses-foto.dto';
-import { UpdateBusesFotoDto } from './dto/update-buses-foto.dto';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
@@ -16,19 +14,4 @@ export class BusesFotosService {
     return results;
   }
 
-  findAll() {
-    return `This action returns all busesFotos`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} busesFoto`;
-  }
-
-  update(id: number, updateBusesFotoDto: UpdateBusesFotoDto) {
-    return `This action updates a #${id} busesFoto`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} busesFoto`;
-  }
 }
